b02e9e87a3ca092896be6792c1e7edade6542da0
Rohitteja Maddula <maddularohit38@students.itu.edu>
2017-04-24 11:01:35 +0000

    I'M DONE

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -11,7 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302232135) do
+ActiveRecord::Schema.define(version: 20160421221316) do
+
+  create_table "dogs", force: true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
